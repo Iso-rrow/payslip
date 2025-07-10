7a913423ab59d94c482a31cbe774fcad0e77fd8a
Iso-rrow <jima33599@gmail.com>
2025-07-10 20:27:18 +0800

    kupal

--- admin/pages/attendance.php
+++ admin/pages/attendance.php
@@ -120,11 +120,9 @@ $result = $stmt->get_result();
 
 
 
-<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css"> -->
+
 
 <script>
     // $(document).ready(function() {
